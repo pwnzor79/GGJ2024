8bb1d10c5216703f01e0b0f116927df7ea45356d
Robert Breglio <43708172+pwnzor79@users.noreply.github.com>
2024-01-27 13:59:17 -0700

    dictionary check

--- BobbyAssets/BobbyAudio.gd
+++ BobbyAssets/BobbyAudio.gd
@@ -1,12 +1,24 @@
 extends AudioStreamPlayer2D
 
+@export var playerText: RichTextLabel
+@export var bobbyText: RichTextLabel
+@export var dictionary: String
+
+#https://forum.godotengine.org/t/how-to-import-and-read-text/21936
+@onready var bobbyDictionary = "res://BobbyAssets/words.txt"
+
+var file = FileAccess.open("res://BobbyAssets/words.txt", FileAccess.READ)
+
 #var questionPick int: = 0
-var questionText: String = "test string"
 var answerText: String = ""
 var timeTrim: float = 0.3
+var isTalking: bool = false
+var dictionaryArray = [" "]
+var csvFile: String = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_loadFile()
 	#_askQuestion()
 	pass # Replace with function body.
 
@@ -16,8 +28,33 @@ func _process(delta):
 	#_askQuestion()
 	pass
 
-func _input(event):
+func _loadFile() -> void: #it is redundant to call this file if 
+#https://forum.godotengine.org/t/load-variable-from-file/321/2
+	if file == null:
+		printerr("Could not load example.csv: %s" % error_string(FileAccess.get_open_error()))
+		return
+	print("begin processing csv data")
 
+	# get header line - remove/comment next line if no header in csv file
+	var _discarded_header_line = file.get_line()
+	
+	while !file.eof_reached():
+		var csv_data := file.get_csv_line()
+		# process csv data - for example just print it
+		#print("CSV data is %s" % ", ".join(csv_data))
+		#add csv data to our array
+		#csv_data.erase(0, 2)
+		dictionaryArray.resize(dictionaryArray.size() + 1)
+		dictionaryArray[dictionaryArray.size()-1] = csv_data[0]
+		#dictionaryArray[dictionaryArray.size()-1].erase(0,2)
+		#print(dictionaryArray[dictionaryArray.size()-1])
+
+	
+	print("done processing csv data")
+	print(dictionaryArray)
+
+
+func _input(event):
 
 	#print(event.as_text())
 	if Input.is_anything_pressed():
@@ -75,33 +112,37 @@ func _input(event):
 			"z", "Z":
 				answerText += "z"
 			"Enter":
-				_sayText(answerText)
+				if isTalking:
+					print("don't interrupt me")
+				else:
+					_sayText(answerText)
 			"Backspace":
 				answerText[answerText.length()-1] = ""
 			"Space":
 				answerText += " "
 		print(answerText)
+		playerText.text = answerText
 
 func _askQuestion():
 	var questionPick := randi_range(0, 5)
 	print(questionPick)
 	if(questionPick == 0):
-		self.questionText = ("what is your name? ")
-		_sayText(self.questionText)
+		bobbyText.text = ("what is your name? ")
+		_sayText(bobbyText.text)
 	elif(questionPick == 1):
-		self.questionText = ("what is your quest? ")
-		_sayText(self.questionText)
+		bobbyText.text = "what is your quest?"
+		_sayText(bobbyText.text)
 	elif(questionPick == 2):
-		self.questionText = ("sd ")
+		bobbyText.text = ("sd ")
 	elif(questionPick == 3):
-		self.questionText = ("doing question 3 ")
-		_sayText(self.questionText)
+		bobbyText.text = ("doing question 3 ")
+		_sayText(bobbyText.text)
 	elif(questionPick == 4):
-		self.questionText = ("aaaaadaaaaaaaaaaaaaaaaaaaaaaaaaaa ")
-		_sayText(self.questionText)
+		bobbyText.text = ("aaaaadaaaaaaaaaaaaaaaaaaaaaaaaaaa ")
+		_sayText(bobbyText.text)
 	elif(questionPick == 5):
-		self.questionText = ("doing question 5 ")
-		_sayText(self.questionText)
+		bobbyText.text = ("doing question 5 ")
+		_sayText(bobbyText.text)
 
 #https://godotforums.org/d/24845-how-to-change-the-stream-of-an-audiostreamplayer-via-gdscript
 #this shows how to change the sound this plays
@@ -109,6 +150,13 @@ func _askQuestion():
 #https://forum.godotengine.org/t/get-first-n-characters-from-string/27447
 # this shows how to parse the text
 func _sayText(question: String):
+	isTalking = true
+	
+	if(dictionaryArray.has(question)):
+		print("that is in the dictionary")
+	else:
+		print("that is not in the dictionary")
+	
 	for n in question.length()-1:
 		
 		#print(question[n])
@@ -116,7 +164,6 @@ func _sayText(question: String):
 		#play the sound
 		#https://www.reddit.com/r/godot/comments/12zia1x/how_to_add_small_delay_to_a_one_off_sound_play/
 		#this shows how to hold this process until the sound finishes.
-		
 		
 		match question[n]:
 			"a", "A":
@@ -249,7 +296,8 @@ func _sayText(question: String):
 				self.playing = true
 				await get_tree().create_timer(self.stream.get_length() - timeTrim).timeout
 		#hold until the sound is done
-	
+		
+	isTalking = false
 	
 	
 	
