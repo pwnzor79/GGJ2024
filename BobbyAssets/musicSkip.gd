c475dd5b51130bba44749fd7f4577aa776de7134
Robert Breglio <43708172+pwnzor79@users.noreply.github.com>
2024-01-28 15:51:20 -0700

    added skip beginning method

--- BobbyAssets/musicSkip.gd
+++ BobbyAssets/musicSkip.gd
@@ -1,17 +1,19 @@
 extends Node2D
 
-@export var speed = 600
 @export var backgroundMusic: AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	backgroundMusic.playing = false
+	#skipBeginning(4)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
+
 func skipBeginning(time):
 	backgroundMusic.play(time)
+		
 		
--- MaddieGame/Scenes & Scripts/MainScript.gd
+++ MaddieGame/Scenes & Scripts/MainScript.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 @export var enemyToSpawn: PackedScene
+@export var backgroundMusic: AudioStreamPlayer2D
 var game_over = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$HUD/ControlsMessage.show()
-	$BGM.play()
+	#$BGM.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -29,3 +30,9 @@ func _on_player_game_over():
 # Fades out controls message on timeout
 func _on_message_timer_timeout():
 	$HUD/ControlsMessage.hide()
+	
+	
+func skipBeginning(time):
+	backgroundMusic.play(time)
+		
+
