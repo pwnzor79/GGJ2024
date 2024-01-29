7a5fa21c488faba49020b2ed0b6945cc718e3f96
Ian Warren <103790584+Duckkk819@users.noreply.github.com>
2024-01-28 20:02:09 -0700

    working version i think

--- Scenes/Area3D.gd
+++ Scenes/Area3D.gd
@@ -4,7 +4,7 @@ extends Area3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("/root/Control/CadeScene/enemy/CharacterBody3D").set_process(false)
-	willie_scream_stream = get_node("/root/CadeScene/enemy/CharacterBody3D/CollisionShape3D/willie_scream")
+	willie_scream_stream = get_node("/root/Control/CadeScene/enemy/CharacterBody3D/CollisionShape3D/willie_scream")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,7 +21,7 @@ func _on_body_entered(body):
 	if (!chase_active):
 		print("Setting hunt active")
 		get_node("/root/Control/CadeScene/enemy/CharacterBody3D").set_process(true)
-		var spawn_pos = get_node("/root/CadeScene/enemy/spawn").position
+		var spawn_pos = get_node("/root/Control/CadeScene/enemy/spawn").position
 		get_node("/root/Control/CadeScene/enemy/CharacterBody3D").position = spawn_pos
 		chase_active = true
 		get_node("/root/Control/CadeScene/player/ChaseStart").play()
