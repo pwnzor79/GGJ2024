1e3136e89ae852ad91392aeffd25900a5c5e4b52
Ian Warren <103790584+Duckkk819@users.noreply.github.com>
2024-01-29 16:12:48 -0700

    scene switching

--- Contol.gd
+++ Contol.gd
@@ -4,26 +4,31 @@ extends Control
 @export var scenes = {}
 @export var scenesArray = []
 @export var finalScene = []
-var timeBetweenScenes = Vector2(25,30)
-var minScenesToVisit = 6
-var numScenesVisited = 0
+@export var sceneCodes = {}
+@export var cadeSceneCodes = {}
+@export var doriSceneCodes = {}
+@export var timeBetweenScenes = Vector2(25,30)
+#var minScenesToVisit = 6
+#var numScenesVisited = 0
 
+var currentScene = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	scenesArray.shuffle()
 	for sceneTree in scenesArray:
 		await $AnimatedSprite2D.do_static(0.5)
-		print(sceneTree)
 		var scene = sceneTree.instantiate()
-		print(scene)
 		self.add_child(scene)
-		# if visited set music to last position
+		currentScene = scene
+		# if visited set music to last position (doesnt work lol)
 		if scenes.get(sceneTree) > 0:
 			scene.backgroundMusic.play(scenes.get(scene))
 		else:
 			scene.backgroundMusic.play()
 		var time = randf_range(timeBetweenScenes.x, timeBetweenScenes.y)
 		await get_tree().create_timer(time).timeout
+		if (!is_instance_valid(scene)):
+			return
 		scenes[scene] = scene.backgroundMusic.get_playback_position()
 		scene.queue_free()
 		Input.set_custom_mouse_cursor(null)
@@ -33,7 +38,49 @@ func _ready():
 	add_child(finalScene[0].instantiate())
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _input(event):
+	if !event.is_pressed():
+		return
+	for code in sceneCodes:
+		if event.as_text().to_lower() == "ctrl+" + code:
+			var i = 1
+			while(is_instance_valid(self.get_child(i))):
+				get_child(i).queue_free()
+				i+=1
+			self.PRESET_TOP_LEFT
+			Input.set_custom_mouse_cursor(null)
+			await $AnimatedSprite2D.do_static(0.5)
+			var scene = sceneCodes[code].instantiate()
+			add_child(scene)
+			scene.backgroundMusic.play()
+	
+	for code in cadeSceneCodes:
+		if event.as_text().to_lower() == "ctrl+" + code:
+			var i = 1
+			while(is_instance_valid(self.get_child(i))):
+				get_child(i).queue_free()
+				i+=1
+			Input.set_custom_mouse_cursor(null)
+			self.PRESET_CENTER
+			await $AnimatedSprite2D.do_static(2)
+			var scene = cadeSceneCodes[code].instantiate()
+			add_child(scene)
+
+	for code in doriSceneCodes:
+		if event.as_text().to_lower() == "ctrl+" + code:
+			var i = 1
+			while(is_instance_valid(self.get_child(i))):
+				get_child(i).queue_free()
+				i+=1
+			Input.set_custom_mouse_cursor(null)
+			self.PRESET_TOP_LEFT
+			await $AnimatedSprite2D.do_static(0.5)
+			var scene = doriSceneCodes[code].instantiate()
+			add_child(scene)
+		
+	#if Input.is_key_pressed(KEY_CTRL):
+		
 
 
 
